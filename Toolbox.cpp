407319037a232ff15c2798da3943875ba3fe1fe1
Deaboy <danieleandrus@gmail.com>
2015-04-09 19:15:54 -0600

    Added inverse Fourier transform code.

--- Toolbox.cpp
+++ Toolbox.cpp
@@ -12,12 +12,22 @@
  * Returns
  *          true if successful, false if not
  ******************************************************************************/
-bool FourierTransform(Image& image, int ** frequencies)
+bool FourierTransform(Image& image, double ** frequencies)
 {
-    int ** real = new int[image.Height()][image.Width()];
-    int ** imag = new int[image.Height()][image.Width()];
     double theta;
+    
+    // Allocate 2D arrays
+    double ** real = new double * [image.Height()];
+    double ** imag = new double * [image.Height()];
+    double ** i_frequencies = new double * [image.Height()];
+    for (int i = 0; i < image.Height(); i++)
+    {
+        real[i] = new double[image.Width()];
+        imag[i] = new double[image.Width()];
+        i_frequencies = new double[image.Width()];
+    }
 
+    // Perform Fourier transform along the rows
     for (int r = 0; r < image.Height(); r++)
     {
         for (int c = 0; c < image.Width(); c++)
@@ -33,8 +43,7 @@ bool FourierTransform(Image& image, int ** frequencies)
         }
     }
 
-    int ** i_frequencies = new int[image.Height()][image.Width()];
-
+    // Perform Fourier transform along the columns
     for (int r = 0; r < image.Height(); r++)
     {
         for (int c = 0; c < image.Width(); c++)
@@ -49,6 +58,17 @@ bool FourierTransform(Image& image, int ** frequencies)
             }
         }
     }
+    
+    // Deallocate 2D arrays
+    for (int i = 0; i < image.Height(); i++)
+    {
+        delete [] real[i];
+        delete [] imag[i];
+        delete [] i_frequencies[i];
+    }
+    delete [] real;
+    delete [] imag;
+    delete [] i_frequencies;
 }
 
 /***************************************************************************//**
@@ -63,7 +83,66 @@ bool FourierTransform(Image& image, int ** frequencies)
  * Returns
  *          true if successful, false if not
  ******************************************************************************/
-bool InverseFourierTransform(Image& image, int ** frequencies)
+bool InverseFourierTransform(Image& image, double ** frequencies)
 {
+    double theta;
+    
+    // Allocate 2D arrays
+    double ** real = new double * [image.Height()];
+    double ** imag = new double * [image.Height()];
+    double ** i_frequencies = new double * [image.Height()];
+    for (int i = 0; i < image.Height(); i++)
+    {
+        real[i] = new double[image.Width()];
+        imag[i] = new double[image.Width()];
+        i_frequencies = new double[image.Width()];
+    }
+    
+    // Perform inverse Fourier transform along the rows
+    for (int r = 0; r < image.Height(); r++)
+    {
+        for (int c = 0; c < image.Width(); c++)
+        {
+            real[r][c] = 0;
+            imag[r][c] = 0;
+            for (int i = 0; i <image.Width(); i++)
+            {
+                theta = (2 * M_PI * c * i )/ image.Width();
+                real[r][c] += -1 * image[r][c].Intensity() * cos(theta);
+                imag[r][c] += image[r][c].Intensity() * sin(theta);
+            }
+            real[r][c] /= image.Width();
+            imag[r][c] /= image.Width();
+        }
+    }
 
+    // Perform inverse Fourier transform along the columns
+    for (int r = 0; r < image.Height(); r++)
+    {
+        for (int c = 0; c < image.Width(); c++)
+        {
+            frequencies[r][c] = 0;
+            i_frequencies[r][c] = 0;
+            for (int i = 0; i <image.Height(); i++)
+            {
+                theta = (2 * M_PI * r * i )/ image.Height();
+                frequencies[r][c] += -1 * real[r][c] * cos(theta) + imag[r][c] * sin(theta);
+                i_frequencies[r][c] += real[r][c] * sin(theta) + imag[r][c] * cos(theta);
+            }
+            frequencies[r][c] /= image.Height();
+            i_frequencies[r][c] /= image.Height();
+        }
+    }
+    
+    // Deallocate 2D arrays
+    for (int i = 0; i < image.Height(); i++)
+    {
+        delete [] real[i];
+        delete [] imag[i];
+        delete [] i_frequencies[i];
+    }
+    delete [] real;
+    delete [] imag;
+    delete [] i_frequencies;
 }
+
